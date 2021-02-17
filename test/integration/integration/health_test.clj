667dab1983672209031d44120a74ccf0a08ecdd3
Eduardo Ferreira <eduardoofe@gmail.com>
2021-02-17 11:55:45 -0300

    Create integration.helper

--- test/integration/integration/health_test.clj
+++ test/integration/integration/health_test.clj
@@ -1,15 +1,11 @@
 (ns integration.health-test 
   (:require [clojure.test :refer :all]
             [io.pedestal.test :refer :all]
-            [io.pedestal.http :as bootstrap]
             [matcher-combinators.test :refer [match?]]
-            [clojure-service.server :as server]))
-
-(def service
-  (::bootstrap/service-fn (bootstrap/create-servlet (server/build-service-map))))
+            [integration.helper :as helper]))
 
 (deftest health-test
-  (let [response (response-for service :get "/api/health")]
+  (let [response (response-for helper/service :get "/api/health")]
     (is (match? {:status 200
                  :body "{\"message\":\"I have a dream - Martin Luther King, Jr.\"}"}
                 response))))
---
+++ test/integration/integration/helper.clj
@@ -0,0 +1,3 @@
+(ns integration.helper
+  (:require [io.pedestal.http :as bootstrap]
+            [clojure-service.server :as server]))

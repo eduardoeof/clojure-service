61542a6bee5a9287f47f8ab90421ad82b8e53ad9
Eduardo Ferreira <eduardoofe@gmail.com>
2021-05-23 17:00:20 -0300

    Fix health integration test

--- test/integration/integration/health_test.clj
+++ test/integration/integration/health_test.clj
@@ -1,11 +1,20 @@
 (ns integration.health-test 
   (:require [clojure.test :refer :all]
-            [io.pedestal.test :refer :all]
             [matcher-combinators.test :refer [match?]]
-            [integration.helper :as helper]))
+            [clojure-service.component :as component]
+            [integration.util.http :refer [http-get edn->json]]))
+
+(def components (atom nil))
+
+(defn fixtures-once [test-case]
+  (reset! components (component/create-and-start)) 
+  (test-case)
+  (reset! components nil))
+
+(use-fixtures :once fixtures-once)
 
 (deftest health-test
-  (let [response (response-for (helper/create-service) :get "/api/health")]
+  (let [response (http-get "/api/health" @components)]
     (is (match? {:status 200
-                 :body "{\"message\":\"I have a dream - Martin Luther King, Jr.\"}"}
+                 :body (edn->json {:message "I have a dream - Martin Luther King, Jr."})} 
                 response))))
